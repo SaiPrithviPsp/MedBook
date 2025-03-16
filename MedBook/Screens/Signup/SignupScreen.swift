ec4f2676dc3a890d2ba6e8c2ac583122dad0a036
SaiPrithviPsp <saiprithvipsp@gmail.com>
2025-03-17 02:00:12 +0530

    minor fixes

--- MedBook/Screens/Signup/SignupScreen.swift
+++ MedBook/Screens/Signup/SignupScreen.swift
@@ -12,95 +12,109 @@ struct SignupScreen: View {
     @EnvironmentObject var router: RouterViewModel
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            Text("Welcome")
-                .font(.title)
-                .fontWeight(.bold)
-            Text("Sign up to continue")
-                .foregroundColor(.gray)
-            
+        ScrollView {
             VStack(alignment: .leading, spacing: 16) {
-                VStack(alignment: .leading, spacing: 4) {
-                    TextField("Email", text: $viewModel.email)
-                        .autocapitalization(.none)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                    if let error = viewModel.emailError {
-                        Text(error)
-                            .foregroundColor(.red)
-                            .font(.caption)
-                    }
-                }
+                Text("Welcome")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Text("Sign up to continue")
+                    .foregroundColor(.gray)
                 
-                VStack(alignment: .leading, spacing: 4) {
-                    SecureField("Password", text: $viewModel.password)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                    if let error = viewModel.passwordError {
-                        Text(error)
-                            .foregroundColor(.red)
-                            .font(.caption)
+                VStack(alignment: .leading, spacing: 16) {
+                    VStack(alignment: .leading, spacing: 4) {
+                        TextField("Email", text: $viewModel.email)
+                            .autocapitalization(.none)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        if let error = viewModel.emailError {
+                            Text(error)
+                                .foregroundColor(.red)
+                                .font(.caption)
+                        }
                     }
                     
-                    if let signUpError = viewModel.signUpError {
-                        Text(signUpError)
-                            .foregroundColor(.red)
-                            .padding(.vertical, 8)
-                    }
-                    
-                    // Password requirements
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("Password requirements:")
-                            .font(.caption)
-                            .foregroundColor(.gray)
-                        
-                        HStack {
-                            Image(systemName: viewModel.hasMinLength ? "checkmark.square.fill" : "square")
-                                .foregroundColor(viewModel.hasMinLength ? .green : .gray)
-                            Text("At least 8 characters")
+                    VStack(alignment: .leading, spacing: 4) {
+                        SecureField("Password", text: $viewModel.password)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        if let error = viewModel.passwordError {
+                            Text(error)
+                                .foregroundColor(.red)
                                 .font(.caption)
                         }
                         
-                        HStack {
-                            Image(systemName: viewModel.hasUppercase ? "checkmark.square.fill" : "square")
-                                .foregroundColor(viewModel.hasUppercase ? .green : .gray)
-                            Text("Must contain an uppercase letter")
-                                .font(.caption)
+                        if let signUpError = viewModel.signUpError {
+                            Text(signUpError)
+                                .foregroundColor(.red)
+                                .padding(.vertical, 8)
                         }
                         
-                        HStack {
-                            Image(systemName: viewModel.hasSpecialCharacter ? "checkmark.square.fill" : "square")
-                                .foregroundColor(viewModel.hasSpecialCharacter ? .green : .gray)
-                            Text("Contains a special character")
+                        // Password requirements
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Password requirements:")
                                 .font(.caption)
-                        }
-                    }
-                    .padding(.top, 8)
-                }
-                
-                Spacer()
-                
-                VStack(alignment: .leading, spacing: 8) {
-                    if !viewModel.countryList.isEmpty {
-                        Picker("Country", selection: $viewModel.selectedCountry) {
-                            ForEach(viewModel.countryList, id: \.self) { country in
-                                Text(country)
-                                    .tag(country)
+                                .foregroundColor(.gray)
+                            
+                            HStack {
+                                Image(systemName: viewModel.hasMinLength ? "checkmark.square.fill" : "square")
+                                    .foregroundColor(viewModel.hasMinLength ? .green : .gray)
+                                Text("At least 8 characters")
+                                    .font(.caption)
+                            }
+                            
+                            HStack {
+                                Image(systemName: viewModel.hasUppercase ? "checkmark.square.fill" : "square")
+                                    .foregroundColor(viewModel.hasUppercase ? .green : .gray)
+                                Text("Must contain an uppercase letter")
+                                    .font(.caption)
+                            }
+                            
+                            HStack {
+                                Image(systemName: viewModel.hasSpecialCharacter ? "checkmark.square.fill" : "square")
+                                    .foregroundColor(viewModel.hasSpecialCharacter ? .green : .gray)
+                                Text("Contains a special character")
+                                    .font(.caption)
                             }
                         }
-                        .pickerStyle(.wheel)
-                        .frame(height: 150)
+                        .padding(.top, 8)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .leading, spacing: 8) {
+                        if !viewModel.countryList.isEmpty {
+                            Picker("Country", selection: $viewModel.selectedCountry) {
+                                ForEach(viewModel.countryList, id: \.self) { country in
+                                    Text(country)
+                                        .tag(country)
+                                }
+                            }
+                            .pickerStyle(.wheel)
+                            .frame(height: 150)
+                        }
                     }
                 }
-            }
-            .padding(.bottom, 16)
-            
-            PrimaryCta(text: "Sign up", rightIcon: Image(systemName: "arrow.right"), isEnabled: viewModel.isPrimaryCtaEnabled) {
-                viewModel.didTapSignUpButton()
+                .padding(.bottom, 16)
+                
+                PrimaryCta(text: "Sign up", rightIcon: Image(systemName: "arrow.right"), isEnabled: viewModel.isPrimaryCtaEnabled) {
+                    viewModel.didTapSignUpButton()
+                }
+                .padding()
             }
             .padding()
         }
-        .padding()
         .background(Color.primaryBgColor)
         .navigationTitle("Sign up")
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    router.goBack()
+                } label: {
+                    Image(systemName: "chevron.left")
+                        .foregroundColor(.primary)
+                }
+            }
+        }
+        .ignoresSafeArea(.keyboard, edges: .bottom)
         .onAppear {
             viewModel.onViewAppear()
         }
